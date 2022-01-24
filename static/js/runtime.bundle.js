43e6d3a92a0ae8b3ff192b6a152bc05523735a1b
Arif Sardar <as2048282@gmail.com>
2022-01-24 20:34:05 +0530

    docs

--- static/js/runtime.bundle.js
+++ static/js/runtime.bundle.js
@@ -137,7 +137,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("daff1eb37db841a3736f")
+/******/ 		__webpack_require__.h = () => ("2072d3a55008625555a2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/harmony module decorator */
